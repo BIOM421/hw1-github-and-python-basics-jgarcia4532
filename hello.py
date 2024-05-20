d8f30e2004df5c6cdae442e002922e486743f93f
Jayden Garcia <jg4532@nmsu.edu>
2024-05-20 12:11:28 -0600

    My First Commit

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
+# "hello_world()" will take no arguments and will return the string "Hello World!" exactly as shown.
+# "hello_world_n(N)" will take one argument (N) and will return the string "Hello World! Hello World! Hello World! ... Hello World!" with exactly N repetitions.
+
 def hello_world():
-	return "Hello"
+	return "Hello World!"
+
+def hello_world_n(N):
+	return "Hello World! " * N
